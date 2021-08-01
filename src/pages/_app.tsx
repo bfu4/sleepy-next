b899939b811a45044610b3bffc848c9ff76a1287
Alistair Smith <alistairsmith01@gmail.com>
2021-08-01 11:48:46 +0100

    feat: Implement SWR fetcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,28 @@ import '../styles/globals.css';
 import '../styles/index.css';
 import {AppProps} from 'next/app';
 import {Navbar} from '../components/nav';
+import {SWRConfig} from 'swr';
+import {APIResponse} from 'nextkit';
 
 export default function App({Component, pageProps}: AppProps) {
 	return (
-		<>
+		<SWRConfig
+			value={{
+				async fetcher<T>(url: string) {
+					return fetch(url).then(async res => {
+						const body = (await res.json()) as APIResponse<T>;
+
+						if (!body.success) {
+							throw new Error(body.message);
+						}
+
+						return body.data;
+					});
+				},
+			}}
+		>
 			<Navbar />
 			<Component {...pageProps} />
-		</>
+		</SWRConfig>
 	);
 }
