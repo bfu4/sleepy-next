33fe644dbd6f36b3319e8d0a7b768cafb91d2656
bfu4 <bella@sleeeepyti.me>
2021-08-01 04:00:13 -0400

    devicons

---
+++ src/components/icons.tsx
@@ -0,0 +1,12 @@
+/**
+ * Importing devicons into a component.
+ * @param props name
+ * @constructor
+ */
+export function DevIcon(props: {className?: string; name: string; size?: number}) {
+  return (
+    <div className={props.className}>
+      <span className={`devicons devicons-${props.name}`} style={{fontSize: props.size ?? 20}} />
+    </div>
+  );
+}
