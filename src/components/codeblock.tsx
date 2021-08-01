df40e88f49e4197c2b35430c2c1479d2d97fbd84
Alistair Smith <alistairsmith01@gmail.com>
2021-08-01 03:18:43 +0100

    fix-formattingdsadasd

--- src/components/codeblock.tsx
+++ src/components/codeblock.tsx
@@ -4,10 +4,13 @@ import 'prismjs/components/prism-java';
 export function CodeBlock(props: {language: string; data: string}) {
   const lang = props.language.toLowerCase();
   const grammar = languages[lang];
+
   if (!grammar) {
-    return <></>;
+    return null;
   }
+
   const data = highlight(props.data, grammar, props.data);
+
   return (
     <div className={'pt-1 pb-1 pr-2 pl-2 code-block'}>
       <div dangerouslySetInnerHTML={{__html: data}} className={`language-${lang}`} />
