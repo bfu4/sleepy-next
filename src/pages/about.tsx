9c3199efbeda068b1eb9fb9ce59c0a25e3d9ca23
Bella Fusari <bfu@greynoise.io>
2022-06-14 17:51:21 -0400

    it was broken

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,11 +4,10 @@ export default function About() {
 	return (
 		<div className="grid grid-cols-1">
 			<div className="col-start-1">
-			    <Link href="http://104.131.62.11:8080">
-				<a target="_blank">ok!</a>
-			    </Link>
+				<Link href="http://104.131.62.11:8080">
+					<a target="_blank">ok!</a>
+				</Link>
 			</div>
 		</div>
 	);
 }
-
