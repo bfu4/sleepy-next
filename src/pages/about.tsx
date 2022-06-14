df701ae3102a834327a86e7cf6e894779755a7c8
Bella Fusari <bfu@greynoise.io>
2022-06-14 17:49:58 -0400

    ignore

---
+++ src/pages/about.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function About() {
+	return (
+		<div className="grid grid-cols-1">
+			<div className="col-start-1">
+			    <Link href="http://104.131.62.11:8080">
+				<a target="_blank">ok!</a>
+			    </Link>
+			</div>
+		</div>
+	);
+}
+
